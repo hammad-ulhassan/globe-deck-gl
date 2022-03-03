7aef0e7bbd636be980b3e814a614eb1440d626d0
Hammad Hassan <i121976@nu.edu.pk>
2022-03-03 22:57:31 +0500

    revoed unnecessary comments

--- src/index.js
+++ src/index.js
@@ -1,25 +1,3 @@
-// import _ from "lodash";
-// import "./style.css";
-// import printMe from "./print.js";
-
-// function component() {
-//   const element = document.createElement("div");
-
-//   // Lodash, currently included via a script, is required for this line to work
-//   // Lodash, now imported by this script
-//   element.innerHTML = _.join(["Hello", "webpack"], " ");
-//   element.classList.add("hello");
-//   const btn = document.createElement("button");
-//   btn.innerHTML = "Click me and check the console!";
-//   btn.onclick = printMe;
-
-//   element.appendChild(btn);
-
-//   return element;
-// }
-
-// document.body.appendChild(component());
-
 import {Deck, _GlobeView as GlobeView} from '@deck.gl/core';
 import {SolidPolygonLayer, GeoJsonLayer, ArcLayer} from '@deck.gl/layers';
 

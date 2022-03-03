76bbd81b65d5a2b307e6fe0f4014a6c56b050233
Hammad Hassan <i121976@nu.edu.pk>
2022-03-03 16:38:54 +0500

    globe deck-gl init. source moved to isb-pk

---
+++ src/index.js
@@ -0,0 +1,100 @@
+// import _ from "lodash";
+// import "./style.css";
+// import printMe from "./print.js";
+
+// function component() {
+//   const element = document.createElement("div");
+
+//   // Lodash, currently included via a script, is required for this line to work
+//   // Lodash, now imported by this script
+//   element.innerHTML = _.join(["Hello", "webpack"], " ");
+//   element.classList.add("hello");
+//   const btn = document.createElement("button");
+//   btn.innerHTML = "Click me and check the console!";
+//   btn.onclick = printMe;
+
+//   element.appendChild(btn);
+
+//   return element;
+// }
+
+// document.body.appendChild(component());
+
+import {Deck, _GlobeView as GlobeView} from '@deck.gl/core';
+import {SolidPolygonLayer, GeoJsonLayer, ArcLayer} from '@deck.gl/layers';
+
+// source: Natural Earth http://www.naturalearthdata.com/ via geojson.xyz
+const COUNTRIES =
+  'https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_50m_admin_0_scale_rank.geojson'; //eslint-disable-line
+const AIR_PORTS =
+  'https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_10m_airports.geojson';
+
+const INITIAL_VIEW_STATE = {
+  latitude: 20,
+  longitude: 30,
+  zoom: 0
+};
+
+export const deck = new Deck({
+  views: new GlobeView(),
+  initialViewState: INITIAL_VIEW_STATE,
+  controller: true,
+  layers: [
+    // A GeoJSON polygon that covers the entire earth
+    // See /docs/api-reference/globe-view.md#remarks
+    new SolidPolygonLayer({
+      id: 'background',
+      data: [
+        // prettier-ignore
+        [[-180, 90], [0, 90], [180, 90], [180, -90], [0, -90], [-180, -90]]
+      ],
+      opacity: 0.5,
+      getPolygon: d => d,
+      stroked: false,
+      filled: true,
+      getFillColor: [5, 10, 40]
+    }),
+    new GeoJsonLayer({
+      id: 'base-map',
+      data: COUNTRIES,
+      // Styles
+      stroked: true,
+      filled: true,
+      lineWidthMinPixels: 4,
+      getLineColor: [5, 10, 40],
+      getFillColor: [15, 40, 80]
+    }),
+    new GeoJsonLayer({
+      id: 'airports',
+      data: AIR_PORTS,
+      // Styles
+      filled: true,
+      pointRadiusMinPixels: 2,
+      pointRadiusScale: 2000,
+      getPointRadius: f => 11 - f.properties.scalerank,
+      getFillColor: [200, 0, 80, 180],
+      // Interactive props
+      pickable: true,
+      autoHighlight: true,
+      onClick: info =>
+        // eslint-disable-next-line
+        info.object && alert(`${info.object.properties.name} (${info.object.properties.abbrev})`)
+    }),
+    new ArcLayer({
+      id: 'arcs',
+      data: AIR_PORTS,
+      dataTransform: d => d.features.filter(f => f.properties.scalerank < 3),
+      // Styles
+      getSourcePosition: f => [	73.084488, 33.738045], // London 73.084488
+      getTargetPosition: f => f.geometry.coordinates,
+      getSourceColor: [0, 128, 200],
+      getTargetColor: [200, 0, 80],
+      getWidth: 2
+    })
+  ]
+});
+
+// For automated test cases
+/* global document */
+document.body.style.margin = '0px';
+document.body.style.background = '#111';
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    index: "./src/index.js",
+    print: "./src/print.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Development",
+    }),
+  ],
+  devServer: {
+    static: "./dist",
+  },
+  devtool: "inline-source-map",
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+    publicPath: "/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
